c9058d02ed9ecbe87d16db81d7a02d240fe48941
liuby <liuby@hzyb.com>
2023-12-04 16:23:04 +0800

    连接

---
+++ src/views/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="width: 100%;height: 100%;"></div>
+</template>
+
+<script>
+import { supabase } from '../supabase'
+export default {
+    name: 'Test',
+    methods: {
+        async getProfile() {
+            let { data, error } = await supabase
+                .from('t_game')
+                .select('*')
+            console.log(data);
+        }
+    },
+    mounted() {
+        this.getProfile();
+    }
+}
+</script>

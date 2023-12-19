a46f4df721a8ef79b03ef0564c9437fa733c12f3
liuby <liuby@hzyb.com>
2023-12-19 18:00:04 +0800

    页面布局

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus, { size: 'small', zIndex: 3000, locale })
 
 app.mount('#app')

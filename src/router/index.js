fafed1b4c823af536b643f1bea42b2759f9b1570
洗洗睡 <9767799+wash-and-sleep1@user.noreply.gitee.com>
2023-12-23 17:41:34 +0800

    构建测试

--- src/router/index.js
+++ src/router/index.js
@@ -18,8 +18,8 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/gamelist',
-      name: 'gamelist',
+      path: '/test',
+      name: 'test',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.

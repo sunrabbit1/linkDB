6af180df6cbc4d5eeb59309b97a73a4f9b12de73
洗洗睡 <9767799+wash-and-sleep1@user.noreply.gitee.com>
2023-12-23 18:09:13 +0800

    构建测试

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,28 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-        path: '/gamelist',
-        name: 'gamelist',
-        component: () => import('../views/GameListView.vue')
-    },
-    {
-      path: '/:pathMatch(.*)',
-      component: () => import('../views/NotFound.vue')
-    }
-  ]
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/AboutView.vue')
+        },
+        {
+            path: '/gamelist',
+            name: 'gamelist',
+            component: () => import('../views/GameListView.vue')
+        },
+        {
+            path: '/:pathMatch(.*)',
+            component: () => import('../views/NotFound.vue')
+        }
+    ]
 })
 
 export default router
